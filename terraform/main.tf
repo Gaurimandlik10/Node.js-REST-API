acbcc9f9c2786384e109ce46523b51065d4179e3
Gauri Pravin Mandlik <125661416+Gaurimandlik10@users.noreply.github.com>
2026-06-12 11:54:49 +0530

    Change EKS node group instance type to c7i-flex.large

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,7 +62,7 @@ module "eks"{
 
      eks_managed_node_groups = {
         default ={
-            instance_types = ["t3.medium"]
+            instance_types = ["c7i-flex.large"]
 
             min_size = 1
             max_size = 3
